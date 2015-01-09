ced122684b8af00994ab2b089f22d5f4f0032102
Michael Yang <mikkyangg@gmail.com>
2015-01-09 17:38:27 +0900

    Rename macro

--- src/mat.rs
+++ src/mat.rs
@@ -1,7 +1,7 @@
 // Copyright 2014 Michael Yang. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
-#![macro_escape]
+#![macro_use]
 
 use std::fmt;
 use std::mem;
